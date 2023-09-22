34010189ff937ec9d35d36469e6b22a77b87ceb7
Alan Jason Lewandowski <85626827+LewanX@users.noreply.github.com>
2023-09-22 00:16:30 -0300

    Agregar archivos de proyecto.

---
+++ RickAndMorty/Data/Models/Characters.cs
@@ -0,0 +1,16 @@
+﻿namespace RickAndMorty.Data.Models
+{
+    public class Characters
+    {
+        public Info info { get; set; }
+        public List<Character> results { get; set; }
+    }
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+
+    }
+}
---
+++ RickAndMorty/Data/Services/CharacterService.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RickAndMorty.Data.Models;
+
+namespace RickAndMorty.Data.Services
+{
+    public class CharacterService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CharacterService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://rickandmortyapi.com/api/");
+        }
+
+        public async Task<Characters> GetAllCharacters()
+        {
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync("character/");
+                httpResponseMessage.EnsureSuccessStatusCode();
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                Characters characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+                return characters;
+            }
+            catch (Exception ex)
+            {
+                // Maneja aquí la excepción de manera adecuada si lo deseas.
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
