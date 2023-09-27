e5980276ae381420ebf968aac883b1862906b043
Alan Jason Lewandowski <85626827+LewanX@users.noreply.github.com>
2023-09-27 14:02:37 -0300

    feat(callbacks):

--- RickAndMorty/Data/Services/CharacterService.cs
+++ RickAndMorty/Data/Services/CharacterService.cs
@@ -13,6 +13,26 @@ namespace RickAndMorty.Data.Services
             _httpClient.BaseAddress = new Uri("https://rickandmortyapi.com/api/");
         }
 
+     
+
+             public async Task<Characters> GetCharacterByName(int id=0,string name="",string status = "")
+        {
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync($"character/?page={id}&name={name}&status={status}&gender=&species=");
+                httpResponseMessage.EnsureSuccessStatusCode();
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                Characters characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+                return characters;
+            }
+            catch (Exception ex)
+            {
+                // Maneja aquí la excepción de manera adecuada si lo deseas.
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
         public async Task<Characters> GetAllCharacters(int id)
         {
             try
